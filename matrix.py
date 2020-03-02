212b64ec22900f64586abb576a2169836bb993bc
mmiah00 <mmiah00@stuy.edu>
2020-03-02 06:27:31 +0000

    .

--- matrix.py
+++ matrix.py
@@ -11,12 +11,9 @@ import math
 
 def make_translate( x, y, z ):
     ans = ident (new_matrix ())
-    ans[0][3] = x
-    ans[1][3] = y
-    ans[2][3] = z 
-    # params = [x,y,z]
-    # for i in range (len (params)):
-    #     ans [i][3] = params[i]
+    params = [x,y,z]
+    for i in range (len (params)):
+        ans [i][3] = params[i]
     return ans
 
 def make_scale( x, y, z ):
